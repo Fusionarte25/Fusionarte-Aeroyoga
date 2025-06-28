f3eee81e17bef54e58b0a4bd1f982ebe79990fab
Luis Girao <luis.oscar.girao@gmail.com>
2025-06-28 01:15:29 +0000

    lo abri en mi android y no em aparece la ventana emergente para descarga

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
+  register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
 
