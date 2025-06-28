f678f0f3c207c3bfe053ec20f8efbb183823cdd3
Luis Girao <luis.oscar.girao@gmail.com>
2025-06-28 00:46:04 +0000

    se puede hacer Aplicación Web Progresiva

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: 'Fusionarte',
   description: 'Gestiona tus reservas de clases de Aeroyoga con facilidad.',
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Fusionarte',
+  },
 };
+
+export const viewport: Viewport = {
+  themeColor: '#3da89f',
+}
 
 export default function RootLayout({
   children,
